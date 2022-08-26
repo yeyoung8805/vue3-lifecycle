0e3a3d0fc38510314685ddef10caec2e5eef1bc8
82227496 <yyk8805@naver.com>
2022-08-26 11:00:56 +0900

    characteristic of each lifecycle

--- src/App.vue
+++ src/App.vue
@@ -10,16 +10,20 @@ export default {
     };
   },
   beforeCreate() {
-    console.log("Before Create!");
+    //데이터 count가 정의되기 전에 실행되는 생명주기
+    console.log("Before Create!", this.count); //undefined
   },
   created() {
-    console.log("Created!");
+    console.log("Created!", this.count);
   },
   beforeMount() {
     console.log("Before Mount!");
+    //HTML에 연결되기 전의 상태이기 때문에 HTML을 조회할 수 없다.
+    console.log(document.querySelector("h1")); //null
   },
   mounted() {
     console.log("Mounted!");
+    console.log(document.querySelector("h1"));
   },
 };
 </script>
