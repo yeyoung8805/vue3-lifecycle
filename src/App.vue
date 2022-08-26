74aef59528bcf82ae97762d3caac97582b18264b
82227496 <yyk8805@naver.com>
2022-08-26 14:06:19 +0900

    computed, handles cache methods

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,7 @@ export default {
     };
   },
   computed: {
+    //computed 메소드는 캐싱 되어 있기 때문에, 한번 계산하면 다시 반복해서 계산하지 않는다.
     reversedMessage() {
       return this.msg.split("").reverse().join("");
     },
