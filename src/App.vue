72c2e1429e0631b57dbc13786a8de027fa64c324
82227496 <yyk8805@naver.com>
2022-08-26 14:40:57 +0900

    base of using activate() method

--- src/App.vue
+++ src/App.vue
@@ -1,33 +1,17 @@
 <template>
-  <h1 @click="changeMessage">{{ msg }}</h1>
-  <h1>{{ reversedMessage }}</h1>
+  <h1 @click="activate">Hello?!</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: "Hello?",
+      isActive: false,
     };
   },
-  computed: {
-    reversedMessage() {
-      return this.msg.split("").reverse().join("");
-    },
-  },
-  watch: {
-    //newValue라는 변경된 값을 나타내는 변수명 자유롭게 사용 가능
-    msg(newValue) {
-      console.log("msg: ", this.newValue);
-    },
-    //마찬가지로 v라는 변경된 값을 나타내는 변수명 자유롭게 사용 가능
-    reversedMessage(v) {
-      console.log("reversedMessage: ", this.v);
-    },
-  },
   methods: {
-    changeMessage() {
-      this.msg = "Good!";
+    activate() {
+      this.isActive = true;
     },
   },
 };
