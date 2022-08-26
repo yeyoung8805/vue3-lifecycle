2e5549e55edcbe48ce74f62d87f891efa288636d
82227496 <yyk8805@naver.com>
2022-08-26 17:52:19 +0900

    list rendering for newFruits

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <ul>
-    <li v-for="(f, i) in fruits" :key="f">{{ f }}-{{ i + 1 }}</li>
+    <li v-for="fruit in newFruits" :key="fruit.id">{{ fruit.name }}</li>
   </ul>
 </template>
 
@@ -9,7 +9,22 @@ export default {
   data() {
     return {
       fruits: ["Apple", "Banana", "Cherry"],
+      // newFruits: [
+      //   { id: 0, name: "Apple" },
+      //   { id: 1, name: "Banana" },
+      //   { id: 2, name: "Cherry" },
+      // ],
     };
+  },
+  computed: {
+    newFruits() {
+      return this.fruits.map((fruit, index) => {
+        return {
+          id: index,
+          name: fruit,
+        };
+      });
+    },
   },
 };
 </script>
