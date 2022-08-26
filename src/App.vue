e6a5ff9caac2faf9200c3d7c658ece0f3bb3e403
82227496 <yyk8805@naver.com>
2022-08-26 14:27:31 +0900

    base of Watch

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,5 @@
 <template>
-  <button @click="add">ADD</button>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
-  <h1>{{ reversedMessage }}</h1>
+  <h1 @click="changeMessage">{{ msg }}</h1>
   <h1>{{ reversedMessage }}</h1>
 </template>
 
@@ -10,32 +7,17 @@
 export default {
   data() {
     return {
-      //Getter , Setter
-      msg: "Hello Computed!",
+      msg: "Hello?",
     };
   },
   computed: {
-    //computed 메소드는 캐싱 되어 있기 때문에, 한번 계산하면 다시 반복해서 계산하지 않는다.
-    //computed is read-only
-    //= Getter
-    // reversedMessage() {
-    //   return this.msg.split("").reverse().join("");
-    // },
-
-    //Getter, Setter
-    reversedMessage: {
-      get() {
-        return this.msg.split("").reverse().join("");
-      },
-      set(value) {
-        console.log(value);
-      },
+    reversedMessage() {
+      return this.msg.split("").reverse().join("");
     },
   },
   methods: {
-    add() {
-      this.reversedMessage += "!?"; //computed 안에 들어있는 메소드 이므로 변경사항 반영이 안된다.
-      // this.msg += "!?"; //data 안에 들어있는 데이터이므로 변경가능 하다.
+    changeMessage() {
+      this.msg = "Good!";
     },
   },
 };
