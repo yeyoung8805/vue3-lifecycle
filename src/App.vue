55fe9cd07c801314c85c068a7e83a8e35b925aa0
82227496 <yyk8805@naver.com>
2022-08-26 15:18:34 +0900

    fold up with fontStyle

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,21 @@
 <template>
-  <h1 :style="{ color, fontSize }" @click="changeStyle">Hello?!</h1>
+  <h1 :style="fontStyle" @click="changeStyle">Hello?!</h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      color: "orange",
-      fontSize: "30px",
+      fontStyle: {
+        color: "orange",
+        fontSize: "30px",
+      },
     };
   },
   methods: {
     changeStyle() {
-      this.color = "red";
-      this.fontSize = "50px";
+      this.fontStyle.color = "red";
+      this.fontStyle.fontSize = "50px";
     },
   },
 };
