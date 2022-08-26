671b9757e6c0c399dad643a714c9bec5072ce189
82227496 <yyk8805@naver.com>
2022-08-26 11:18:48 +0900

    base of template syntax

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,20 @@
 <template>
-  <h1>{{ count }}</h1>
+  <h1 @click="add">
+    {{ msg }}
+  </h1>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      count: 2,
+      msg: "Hello World!",
     };
   },
-  beforeCreate() {
-    //데이터 count가 정의되기 전에 실행되는 생명주기
-    console.log("Before Create!", this.count); //undefined
-  },
-  created() {
-    console.log("Created!", this.count);
-    console.log(document.querySelector("h1")); //null
-  },
-  beforeMount() {
-    console.log("Before Mount!");
-    //HTML에 연결되기 전의 상태이기 때문에 HTML을 조회할 수 없다.
-    console.log(document.querySelector("h1")); //null
-  },
-  mounted() {
-    console.log("Mounted!");
-    console.log(document.querySelector("h1"));
+  methods: {
+    add() {
+      this.msg += "!";
+    },
   },
 };
 </script>
