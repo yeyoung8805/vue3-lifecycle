f227b56549b6e77a5e070eca0ae548ac92a2b20e
82227496 <yyk8805@naver.com>
2022-08-26 10:50:46 +0900

    upgrade package.json version of devDependencies

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,13 @@
 <template>
-  <!-- Remove & use example component! -->
-  <HelloWorld 
-    :msg="message" 
-    @click="handler" />
-  <HelloWorld msg="Good" />
+  <h1>{{ count }}</h1>
 </template>
 
 <script>
-import HelloWorld from '~/components/HelloWorld'
-
 export default {
-  components: {
-    HelloWorld
-  },
   data() {
     return {
-      message: 'Hello Vue!'
-    }
+      count: 2,
+    };
   },
-  methods: {
-    handler() {
-      console.log(this.message)
-      this.message += '!'
-    }
-  }
-}
+};
 </script>
