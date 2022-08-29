b6423cefdffe23c0437029aefdbd3dd9cf76a17a
82227496 <yyk8805@naver.com>
2022-08-29 17:33:42 +0900

    components can ba reusable

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
   <MyBtn />
+  <MyBtn />
+  <MyBtn />
+  <MyBtn />
 </template>
 
 <script>
 import MyBtn from "~/components/MyBtn";
 export default {
   components: {
-    MyBtn
-  }
+    MyBtn,
+  },
 };
 </script>
