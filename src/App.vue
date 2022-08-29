c27e9a0f4c4941714d991aeaf64a688eafa7ebf2
82227496 <yyk8805@naver.com>
2022-08-29 14:14:58 +0900

    add event parameter beside to msg

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <button @click="handler('hi')">Click 1</button>
-  <button @click="handler('what')">Click 2</button>
+  <button @click="handler('hi', $event)">Click 1</button>
+  <button @click="handler('what', $event)">Click 2</button>
 </template>
 
 <script>
 export default {
   methods: {
-    handler(msg) {
+    handler(msg, event) {
       console.log(msg);
+      console.log(event);
     },
   },
 };
