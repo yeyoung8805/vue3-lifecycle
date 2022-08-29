0542ad2d55935656ba8e9125f5938a1710e6276a
82227496 <yyk8805@naver.com>
2022-08-29 14:18:14 +0900

    muli methods need to be seperated with ()

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,15 @@
 <template>
-  <button @click="handler('hi', $event)">Click 1</button>
-  <button @click="handler('what', $event)">Click 2</button>
+  <button @click="handlerA(), handlerB()">Click me!</button>
 </template>
 
 <script>
 export default {
   methods: {
-    handler(msg, event) {
-      console.log(msg);
-      console.log(event);
+    handlerA() {
+      console.log("A");
+    },
+    handlerB() {
+      console.log("B");
     },
   },
 };
