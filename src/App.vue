23795405bd6512104639c5df04197fcbcce56521
82227496 <yyk8805@naver.com>
2022-08-29 14:02:30 +0900

    base of event handling

--- src/App.vue
+++ src/App.vue
@@ -1,35 +1,14 @@
 <template>
   <button @click="handler">Click me!</button>
-  <ul>
-    <li v-for="{ id, name } in newFruits" :key="id">{{ name }}-{{ id }}</li>
-  </ul>
 </template>
 
 <script>
-import shortid from "shortid";
 export default {
-  data() {
-    return {
-      fruits: ["Apple", "Banana", "Cherry"],
-      // newFruits: [
-      //   { id: 0, name: "Apple" },
-      //   { id: 1, name: "Banana" },
-      //   { id: 2, name: "Cherry" },
-      // ],
-    };
-  },
-  computed: {
-    newFruits() {
-      return this.fruits.map((fruit) => ({
-        id: shortid.generate(),
-        name: fruit,
-      }));
-    },
-  },
   methods: {
-    handler() {
-      this.fruits.push("Orange");
+    handler(event) {
+      console.log(event);
     },
   },
 };
 </script>
+>
