312abfa6e190cfc7e835f356145b91f1db3c72c0
82227496 <yyk8805@naver.com>
2022-08-29 17:18:03 +0900

    typeof msg is string, although data is number

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <h1>{{ msg }}</h1>
-  <input type="text" v-model.lazy="msg" />
+  <input type="text" v-model="msg" />
 </template>
 
 <script>
 export default {
   data() {
     return {
-      msg: "Hello world!",
+      msg: 123,
     };
+  },
+  watch: {
+    msg() {
+      console.log(typeof this.msg);
+    },
   },
 };
 </script>
