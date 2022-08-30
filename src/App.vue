5ad86db3906eeea984cd4bc4b6a91248abb18db7
82227496 <yyk8805@naver.com>
2022-08-30 14:13:11 +0900

    base of reactivity

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,13 @@
 <template>
-  <h1 @click="increase">{{ count }} / {{ doubleCount }}</h1>
-  <h1>{{ message }} / {{ reversedMessage }}</h1>
+  <div @click="increase">{{ count }}</div>
 </template>
 
 <script>
 export default {
   data() {
     return {
-      message: "Hello World!",
       count: 0,
     };
-  },
-  computed: {
-    doubleCount() {
-      return this.count * 2;
-    },
-    reversedMessage() {
-      return this.message.split("").reverse().join("");
-    },
   },
   methods: {
     increase() {
