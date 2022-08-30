ef8fc5a30e6aaaf78db0371dbb27a1f9286cabd4
82227496 <yyk8805@naver.com>
2022-08-30 11:17:07 +0900

    new way to get textContent of ref

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,11 @@
 <template>
-  <h1 id="hello">Hello world!</h1>
+  <h1 ref="hello">Hello world!</h1>
 </template>
 
 <script>
 export default {
   mounted() {
-    const h1El = document.querySelector("#hello");
-    console.log(h1El.textContent);
+    console.log(this.$refs.hello.textContent);
   },
 };
 </script>
