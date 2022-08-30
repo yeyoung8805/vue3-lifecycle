34d9bf641b5729f7c760894288646c8be4ebbb80
82227496 <yyk8805@naver.com>
2022-08-30 14:35:11 +0900

    App.vue VS App.Composition.vue

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,14 @@
 <template>
-  <div @click="increase">{{ count }}</div>
+  <h1 @click="increase">{{ count }}</h1>
+  <h1 @click="changeMessage">{{ message }}</h1>
 </template>
 
 <script>
-import { ref } from "vue";
-
 export default {
-  setup() {
-    let count = ref(0);
-    function increase() {
-      count.value += 1;
-    }
+  data() {
     return {
-      count,
-      increase,
+      count: 0,
+      message: "Hello world!",
     };
   },
 };
