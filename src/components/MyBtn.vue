916fa88a5a55e2bd460600a7d1fd48a2deb9d706
82227496 <yyk8805@naver.com>
2022-08-30 09:39:01 +0900

    inherit false,but apply specific class,style attrs

--- src/components/MyBtn.vue
+++ src/components/MyBtn.vue
@@ -2,15 +2,19 @@
   <div class="btn">
     <slot></slot>
   </div>
+  <h1 :class="$attrs.class" :style="$attrs.style"></h1>
 </template>
 
 <script>
 export default {
   inheritAttrs: false,
+  created() {
+    console.log(this.$attrs);
+  },
 };
 </script>
 
-<style scoped lang="scss">
+<style scoped>
 .btn {
   display: inline-block;
   margin: 4px;
