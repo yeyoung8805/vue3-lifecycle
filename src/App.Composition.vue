359f828f4b25e6416c6462dd140ff6c4361a946f
82227496 <yyk8805@naver.com>
2022-08-30 14:47:43 +0900

    add computed composition API

--- src/App.Composition.vue
+++ src/App.Composition.vue
@@ -1,26 +1,34 @@
 <template>
-  <h1 @click="increase">{{ count }}</h1>
-  <h1 @click="changeMessage">{{ message }}</h1>
+  <h1 @click="increase">{{ count }} / {{ doubleCount }}</h1>
+  <h1 @click="changeMessage">{{ message }} / {{ reversedMessage }}</h1>
 </template>
 
 <script>
-import { ref } from "vue";
+import { ref, computed } from "vue";
 export default {
   setup() {
-    let count = ref(0);
+    const count = ref(0);
+    const doubleCount = computed(() => {
+      return count.value * 2;
+    });
     function increase() {
       count.value += 1;
     }
 
-    let message = ref("Hello world!");
+    const message = ref("Hello world!");
+    const reversedMessage = computed(() => {
+      return message.value.split("").reverse().join("");
+    });
     function changeMessage() {
       message.value = "Good?!";
     }
 
     return {
       count,
-      message,
+      doubleCount,
       increase,
+      message,
+      reversedMessage,
       changeMessage,
     };
   },
