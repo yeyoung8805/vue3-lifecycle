47e6971a54b959b23c0c87000146ac73f71f909c
82227496 <yyk8805@naver.com>
2022-08-30 15:10:39 +0900

    MyBtn component

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,19 @@
 <template>
-  <h1 @click="increase">{{ count }} / {{ doubleCount }}</h1>
-  <h1 @click="changeMessage">{{ message }} / {{ reversedMessage }}</h1>
+  <MyBtn class="heropy" style="color: red" color="#ff0000" @hello="log">
+    Apple
+  </MyBtn>
 </template>
 
 <script>
+import MyBtn from "~/components/MyBtn.vue";
+
 export default {
-  data() {
-    return {
-      count: 0,
-      message: "Hello world!",
-    };
-  },
-  computed: {
-    doubleCount() {
-      return this.count * 2;
-    },
-    reversedMessage() {
-      return this.message.split("").reverse().join("");
-    },
-  },
-  watch: {
-    message(newValue) {
-      console.log(newValue);
-    },
-  },
-  created() {
-    console.log(this.message);
-  },
-  mounted() {
-    console.log(this.count);
+  components: {
+    MyBtn,
   },
   methods: {
-    increase() {
-      this.count += 1;
-    },
-    changeMessage() {
-      this.message = "Good?!";
+    log() {
+      console.log("Hello world!");
     },
   },
 };
---
+++ src/components/MyBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-bind="$attrs" class="btn" @click="hello">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+import { onMounted } from "vue";
+export default {
+  inheritAttrs: false,
+  props: {
+    color: {
+      type: String,
+      default: "gray",
+    },
+  },
+  emits: ["hello"],
+  mounted() {
+    console.log(this.color);
+    console.log(this.$attrs);
+  },
+  methods: {
+    hello() {
+      this.$emit("hello");
+    },
+  },
+  
+};
+</script>
