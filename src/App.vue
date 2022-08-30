34914196dbb15c11cbfc8aed73de3abcb3203ffc
82227496 <yyk8805@naver.com>
2022-08-30 11:20:49 +0900

    created VS mounted

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,15 @@
 
 <script>
 export default {
+  created() {
+    //컴포넌트 생성된 직후에는
+    // h1 태그 내용 읽어들이기 전이라, undefinec
+    console.log(this.$refs.hello); //result : undefined
+  },
   mounted() {
+    //컴포넌트 생성되고
+    //HTML 내용이 렌더링 된 이후라서
+    //h1 태그 내용 읽어들일 수 있다.
     console.log(this.$refs.hello);
   },
 };
