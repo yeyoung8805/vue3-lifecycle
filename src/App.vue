e77cca7f9daf8b8c6bf5abf49fa4f5ec838e5c00
82227496 <yyk8805@naver.com>
2022-08-30 10:31:31 +0900

    base of component - Slot

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <MyBtn @heropy="log" @change-msg="logMsg">Banana</MyBtn>
+  <MyBtn>Banana</MyBtn>
 </template>
 
 <script>
@@ -7,15 +7,6 @@ import MyBtn from "~/components/MyBtn";
 export default {
   components: {
     MyBtn,
-  },
-  methods: {
-    log(event) {
-      console.log("Click!!");
-      console.log(event);
-    },
-    logMsg(msg) {
-      console.log(msg);
-    },
   },
 };
 </script>
--- src/components/MyBtn.vue
+++ src/components/MyBtn.vue
@@ -2,23 +2,11 @@
   <div class="btn">
     <slot></slot>
   </div>
-  <h1 @dblclick="$emit('heropy', $event)">ABC</h1>
-  <input type="text" v-model="msg" />
 </template>
 
 <script>
 export default {
-  emits: ["heropy", "changeMsg"],
-  data() {
-    return {
-      msg: "",
-    };
-  },
-  watch: {
-    msg() {
-      this.$emit("changeMsg", this.msg);
-    },
-  },
+
 };
 </script>
 
